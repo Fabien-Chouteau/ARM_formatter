d98df32c43ce21f12a583854856eae4e1ab219fb
Fabien Chouteau <chouteau@adacore.com>
2021-10-05 17:53:25 +0200

    Import for ada-auth.org

---
+++ tool/src/makearm.bat
@@ -0,0 +1,1 @@
+corder arm_formatter /t/z/w/k255/b'arm.bat'/l'ads'/n'adb'/n'ada'/n'a'/c'/zt/t/yqc/w5'/jb'/t/l/yllibc'/q'/w'/r.\object/jx'alkc arm_form'
---
+++ tool/src/mklnk.bat
@@ -0,0 +1,3 @@
+Del .\Object\_Janus.Prj
+Jmanager Add_Project (.\Object\,Default)
+Jmanager Add_Link (.\Object\,Default,C:\JWin321\Rts\Console, JWIN_RTS_CONSOLE)
